666c350871116b9b03c14d7b01d00f31d5856390
Agha-Oko S. MacVincent <amacvincent@claflin.edu>
2019-06-18 01:11:50 -0400

    Updated the scroll function

--- js/src.js
+++ js/src.js
@@ -5,25 +5,20 @@
   $(".menu-toggle").click(function(e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
   });
 
-  // Smooth scrolling using jQuery easing
-$(function () {
-     $('a[href*="#"]').click(function () {
-        var $target = $(this.hash);
-        $target = $target.length ? $target : $('html');
-        var targetOffset = $target.offset().top;
-        $('html,body').animate({scrollTop: targetOffset}, {duration: 1600, easing: 'easeInBounce'});
-     });
+  // Smooth scrolling
+$('.js-scroll-trigger').click(function () {
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 600);
 });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('#sidebar-wrapper .js-scroll-trigger').click(function() {
     $("#sidebar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
   });
 
   // Scroll to top button appear
