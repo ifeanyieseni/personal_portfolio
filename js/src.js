47c521a2bf9a37332147083083102b4a26a55b6d
MacVincent Agha-Oko <amacvincent@claflin.edu>
2019-06-25 22:34:47 -0400

    Update src.js

--- js/src.js
+++ js/src.js
@@ -20,17 +20,6 @@ $('.js-scroll-trigger').click(function () {
     $("#sidebar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
   });
-
-  // Scroll to top button appear
-  $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
   $("form[name='feedback']").validate({
     rules: {
       name: "required",
