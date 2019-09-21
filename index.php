3354b85f8ae5f2f60911f5ed89da048215835826
macvincent <macvincentaghaoko@gmail.com>
2019-09-20 22:26:14 -0400

    feat: Update Resume

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Hi, I am MacVincent Agha-Oko. I love God, family, and country. I am a software developer with plans to one day change the face of the African tech industry.</title>
+  <title>Hi, I am MacVincent Agha-Oko. I am a software developer with plans to one day change the face of the African tech industry.</title>
   <link rel="icon" type="image/jpg" href="img/br.jpg" />
   <!-- Bootstrap Core CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Saira+Stencil+One|Source+Sans+Pro&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/min_style.css">
 </head>
 <body>
   <nav>
